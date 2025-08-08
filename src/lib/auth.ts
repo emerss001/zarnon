e4e8e8e0442f72e5288b54008e4e7863fd7df4e5
emerss001 <emersonnevess80@gmail.com>
2025-08-08 19:16:10 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,12 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
@@ -22,6 +28,6 @@ export const auth = betterAuth({
     modelName: "accountTable",
   },
   verification: {
-    modelName: "verificationTokenTable",
+    modelName: "verificationTable",
   },
 });
